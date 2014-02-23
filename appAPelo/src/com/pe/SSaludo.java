3659526ee43a985862d5da58030cb130b9668045
luantoc82 <luantoc82@gmail.com>
2014-02-22 19:50:58 -0500

    practicas-21.02.2014

--- appAPelo/src/com/pe/SSaludo.java
+++ appAPelo/src/com/pe/SSaludo.java
@@ -4,12 +4,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 
 public class SSaludo extends HttpServlet{
 
+public void init(ServletConfig config) throws ServletException{
+	super.init(config);
+	System.out.println(String.format("%s --> %s","Init Saludo",config.getInitParameter("nick")));
+}
+
 public void doGet(HttpServletRequest request,HttpServletResponse response){
 
-
+System.out.println(String.format("%s --> %s",Thread.currentThread().getId(),getServletConfig().getInitParameter("nick")));
 try{
 PrintWriter out = response.getWriter();
 out.println("<html>");
